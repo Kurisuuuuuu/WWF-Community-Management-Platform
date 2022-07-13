9c97acc9b4ca4fac33d54e8f09a3f6df8eb6e925
bryanbryan77 <71356626+bryanbryan77@users.noreply.github.com>
2022-07-13 22:54:01 +1000

    Log In Screen UI - Edited by Bryan (13/07)

--- app/src/main/java/com/example/infs3605communitymanagement/LoginActivity.java
+++ app/src/main/java/com/example/infs3605communitymanagement/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.login_activity);
         // no dark mode
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         initView();
