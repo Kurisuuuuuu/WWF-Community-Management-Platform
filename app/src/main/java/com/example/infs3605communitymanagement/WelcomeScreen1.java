874b2e41e392f164e9358ce0f5af21df34678e14
bryanbryan77 <71356626+bryanbryan77@users.noreply.github.com>
2022-07-25 14:30:49 +1000

    UI Edit - Bryan (25/07)

---
+++ app/src/main/java/com/example/infs3605communitymanagement/WelcomeScreen1.java
@@ -0,0 +1,14 @@
+package com.example.infs3605communitymanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen1);
+    }
+}
